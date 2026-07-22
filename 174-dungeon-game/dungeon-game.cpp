d4226be7993a4a82437df62f83c1dc05434cb55d
ASHISH SITARE <aallizzwell0726@gmail.com>
2026-07-22 17:30:45 +0530

    Time: 0 ms (100.00%) | Memory: 13.1 MB (52.35%) - LeetSync

--- 174-dungeon-game/dungeon-game.cpp
+++ 174-dungeon-game/dungeon-game.cpp
@@ -4,25 +4,22 @@ public:
         int n = dungeon.size();
         int m = dungeon[0].size();
 
-        vector<vector<int>> dp(n, vector<int>(m));
+        vector<vector<int>> dp(n, vector<int>(m, -1));
 
-        // Base case
         dp[n - 1][m - 1] = max(1, 1 - dungeon[n - 1][m - 1]);
 
-        // Last column
         for (int i = n - 2; i >= 0; i--) {
             dp[i][m - 1] = max(1, dp[i + 1][m - 1] - dungeon[i][m - 1]);
         }
 
-        // Last row
-        for (int j = m - 2; j >= 0; j--) {
-            dp[n - 1][j] = max(1, dp[n - 1][j + 1] - dungeon[n - 1][j]);
+        for (int i = m - 2; i >= 0; i--) {
+            dp[n - 1][i] = max(1, dp[n - 1][i + 1] - dungeon[n - 1][i]);
         }
 
-        // Fill remaining cells
         for (int i = n - 2; i >= 0; i--) {
             for (int j = m - 2; j >= 0; j--) {
                 int need = min(dp[i + 1][j], dp[i][j + 1]);
+
                 dp[i][j] = max(1, need - dungeon[i][j]);
             }
         }
