82f5172fe34120bfd6fe32b58644c61cc850e009
ASHISH SITARE <aallizzwell0726@gmail.com>
2026-07-27 14:50:01 +0530

    Time: 1 ms (23.35%) | Memory: 9.8 MB (19.77%) - LeetSync

--- 401-binary-watch/binary-watch.cpp
+++ 401-binary-watch/binary-watch.cpp
@@ -1,12 +1,23 @@
 class Solution {
 public:
+    int count_(int num) {
+        int ans = 0;
+        int i = 0;
+        while (num > 0) {
+            if (num & ( 1 << i)) {
+                ans++;
+            }
+            num /= 2;
+        }
+        return ans;
+    }
     vector<string> readBinaryWatch(int turnedOn) {
         vector<string> validTimes;
         
         for (int h = 0; h < 12; h++) {
             for (int m = 0; m < 60; m++) {
                 
-                if (__builtin_popcount(h) + __builtin_popcount(m) == turnedOn) {
+                if (count_(h) + count_(m) == turnedOn) {
                     
                     string timeStr = to_string(h) + (m < 10 ? ":0" : ":") + to_string(m);
                     validTimes.push_back(timeStr);
