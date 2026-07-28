c37fd77913fd7936387b0dd70c2d7a4872017e10
ASHISH SITARE <aallizzwell0726@gmail.com>
2026-07-28 22:56:14 +0530

    Time: 100 ms (28.79%) | Memory: 69.2 MB (69.23%) - LeetSync

---
+++ 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string smallestPalindrome(string s) {
+        int n = s.size();
+
+        string left = s.substr(0, n / 2);
+        sort(left.begin(), left.end());
+
+        string right = left;
+        reverse(right.begin(), right.end());
+
+        if (n & 1)
+            return left + s[n / 2] + right;
+
+        return left + right;
+    }
+};
