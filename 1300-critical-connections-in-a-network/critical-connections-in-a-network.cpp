d8b871f98e4064ed74582f091542474869e97ff2
ASHISH SITARE <aallizzwell0726@gmail.com>
2026-08-08 19:34:11 +0530

    Time: 305 ms (5.41%) | Memory: 200.3 MB (5.04%) - LeetSync

--- 1300-critical-connections-in-a-network/critical-connections-in-a-network.cpp
+++ 1300-critical-connections-in-a-network/critical-connections-in-a-network.cpp
@@ -1,63 +1,60 @@
 class Solution {
 public:
-    vector<vector<int>> graph;
-    vector<int> tin;
-    vector<int> low;
-    vector<vector<int>> ans;
-    
-    int timer = 0;
 
-    void dfs(int node, int parent) {
-        tin[node] = low[node] = timer++;
+    int n;
+    vector<int> low;
+    vector<int> ti;
+    int timer = 1;
+    vector<vector<int>> bridges;
+
+    void dfs(int node,int parent, vector<vector<int>>& graph, vector<int>& vis) {
+        vis[node] = 1;
+        low[node] = ti[node] = timer++;
 
         for (int x : graph[node]) {
-
             if (x == parent) {
                 continue;
             }
-
-            if (tin[x] == -1) {
-
-                dfs(x, node);
-
-                low[node] = min(low[node], low[x]);
-
-                if (low[x] > tin[node]) {
-                    ans.push_back({node, x});
-                }
-
-            } else {
-
-                low[node] = min(low[node], tin[x]);
-
+            if (!vis[x]) {
+                dfs(x, node, graph, vis);
+                low[node] = min(low[x], low[node]);
             }
-        }
+
+            if (low[x] > ti[node]) {
+                vector<int> move;
+                move.push_back(x);
+                move.push_back(node);
+                bridges.push_back(move);
+            }
+            else {
+                low[node] = min(low[x], low[node]);
+            }
+        } 
+        return;
     }
 
-    vector<vector<int>> criticalConnections(
-        int n,
-        vector<vector<int>>& connections
-    ) {
-
-        graph.assign(n, vector<int>());
-
-        for (auto &p : connections) {
-            int u = p[0];
-            int v = p[1];
+    vector<vector<int>> criticalConnections(int N, vector<vector<int>>& connections) {
+        n = N;
+        int k = connections.size();
+        vector<vector<int>> graph(n + 1, vector<int>());
+    
+        for (int i = 0; i < k; i++) {
+            int u = connections[i][0];
+            int v = connections[i][1];
 
             graph[u].push_back(v);
             graph[v].push_back(u);
         }
-
-        tin.assign(n, -1);
-        low.assign(n, -1);
-
-        for (int i = 0; i < n; i++) {
-            if (tin[i] == -1) {
-                dfs(i, -1);
+        vector<int> vis;
+        vis.assign(n + 1, 0);
+        low.assign(n + 1, 0);
+        ti.assign(n + 1, 0);
+        for (int i = 1; i <= n; i++) {
+            if (!vis[i]) {
+                dfs(1, -1, graph, vis);
             }
         }
 
-        return ans;
+        return bridges;
     }
 };
