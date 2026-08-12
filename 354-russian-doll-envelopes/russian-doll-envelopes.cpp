57ee5067d0287fed2424e8ed4db4fa942db4036c
ASHISH SITARE <aallizzwell0726@gmail.com>
2026-08-12 10:16:23 +0530

    Time: 35 ms (93.39%) | Memory: 81.6 MB (93.82%) - LeetSync

---
+++ 354-russian-doll-envelopes/russian-doll-envelopes.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+
+    int maxEnvelopes(vector<vector<int>>& envelopes) {
+
+        sort(envelopes.begin(), envelopes.end(),
+            [](const auto& a, const auto& b) {
+
+                if (a[0] == b[0]) {
+                    return a[1] > b[1];
+                }
+
+                return a[0] < b[0];
+            }
+        );
+
+        vector<int> lis;
+
+        for (auto &e : envelopes) {
+
+            int height = e[1];
+
+            auto it = lower_bound(
+                lis.begin(),
+                lis.end(),
+                height
+            );
+
+            if (it == lis.end()) {
+                lis.push_back(height);
+            }
+            else {
+                *it = height;
+            }
+        }
+
+        return lis.size();
+    }
+};
